97744f857606f606dcd8f6692ba2be24954fddeb
hexuntao <hexuntao@foxmail.com>
2022-11-26 23:54:46 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  await app.listen(3200, '0.0.0.0');
+
+  console.log(`http://localhost:3200`);
+}
+bootstrap();
